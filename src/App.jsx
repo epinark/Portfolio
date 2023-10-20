0ac7daa512aa7dea2a086e451ea274b7720c0c42
epinark <122348695+epinark@users.noreply.github.com>
2023-10-20 23:09:36 +0200

    add navbar and about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
-
+import Home from "./components/Home";
+import "./styles/Navbar.css";
+import "./styles/About.css";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Navbar />
-      <About />
-      <Skills />
-      <Portfolio />
-      <Contact />
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
   );
 }
