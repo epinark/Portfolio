4b9382597ff898950f0f3bf50064c436c287cc49
epinark <122348695+epinark@users.noreply.github.com>
2023-10-20 14:02:05 +0200

    add basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
