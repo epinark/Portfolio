72bab34d2b9f5558778be27d34f256a4283111b8
epinark <122348695+epinark@users.noreply.github.com>
2023-10-24 12:08:47 +0200

    add home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import sun from "../images/sun.png";
 
 function Home() {
-  return <div>home</div>;
+  const [currenttime, setCurrenttime] = useState(new Date());
+  const greetings = currenttime.getHours();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrenttime(new Date());
+    }, 60000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  let greetingText;
+  let animationClass = "";
+
+  if (greetings < 12) {
+    greetingText = "Good Morning";
+    animationClass = "sunrise-animation";
+  } else if (greetings < 16) {
+    greetingText = "Good Afternoon";
+  } else {
+    greetingText = "Good Evening";
+  }
+
+  return (
+    <div className="header">
+      <h1>
+        {greetingText}, <span>I am Esra</span>
+      </h1>
+      <div className="animation">
+        <img src={sun} alt="Sun" className="sun" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
