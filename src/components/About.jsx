f4e06d85fd2eed4f8bd745ba57884ce097ed7f2f
epinark <122348695+epinark@users.noreply.github.com>
2023-11-04 01:01:10 +0100

    add some animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -65,20 +65,26 @@ function About() {
           </h1>
           <span className="handschrift">Junior Fullstack Web Developer</span>
         </div>
-        <p className="paragraph">
-          <span>
-            I'm<span> Esra,</span>{" "}
-          </span>{" "}
-          <br />I was a math teacher who made a career change when I moved to
-          Germany two years ago, taking it as an opportunity to explore new
-          horizons while leveraging my background. My love for solving
-          challenging problems and exploring different ways of solving them led
-          me to embark on an ambitious journey as a full-stack web developer.
-          Through my education and professional teaching years, I have developed
-          a strong foundation in problem solving,{" "}
-          <span className="handschrift">analytical thinking,</span> and{" "}
-          <span className="handschrift">effective communication.</span>
-        </p>
+        <div className="text-details">
+          <span className="border top-side"></span>
+          <span className="border right-side"></span>
+          <span className="border bottom-side"></span>
+          <span className="border left-side"></span>
+          <p className="paragraph">
+            <span>
+              I'm<span> Esra,</span>{" "}
+            </span>{" "}
+            <br />I was a math teacher who made a career change when I moved to
+            Germany two years ago, taking it as an opportunity to explore new
+            horizons while leveraging my background. My love for solving
+            challenging problems and exploring different ways of solving them
+            led me to embark on an ambitious journey as a full-stack web
+            developer. Through my education and professional teaching years, I
+            have developed a strong foundation in problem solving,{" "}
+            <span className="handschrift">analytical thinking,</span> and{" "}
+            <span className="handschrift">effective communication.</span>
+          </p>
+        </div>
       </div>
     </div>
   );
