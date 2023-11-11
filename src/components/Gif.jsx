d654e58595a90989f8a07f253d515904334116f9
epinark <122348695+epinark@users.noreply.github.com>
2023-11-11 20:27:38 +0100

    change images while scrolling fix

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from "react";
+import image1 from "../images/image1.jpg";
+import image2 from "../images/image2.jpg";
+import image3 from "../images/image3.jpg";
+import image4 from "../images/image4.jpg";
+import image5 from "../images/image5.jpg";
 
 const Gif = () => {
   const [currentImage, setCurrentImage] = useState(1);
+  const [hasStartedScrolling, setHasStartedScrolling] = useState(false);
 
   const handleScroll = () => {
     const scrolled = window.scrollY;
     const windowHeight = window.innerHeight;
 
     const percentage = 6;
-    const imageIndex = Math.ceil(
-      (scrolled / windowHeight) * (100 / percentage)
-    );
 
-    const newIndex = Math.min(Math.max(imageIndex, 1), 5);
+    if (!hasStartedScrolling && scrolled > (windowHeight * percentage) / 100) {
+      setHasStartedScrolling(true);
+    }
 
-    if (newIndex !== currentImage) {
-      setCurrentImage(newIndex);
+    if (hasStartedScrolling) {
+      const imageIndex = Math.ceil(
+        (scrolled / windowHeight) * (100 / percentage)
+      );
+      const newIndex = Math.min(Math.max(imageIndex, 1), 5);
+
+      if (newIndex !== currentImage) {
+        setCurrentImage(newIndex);
+      }
     }
   };
 
@@ -25,14 +37,28 @@ const Gif = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [currentImage]); // Re-run the effect when currentImage changes
+  }, [currentImage, hasStartedScrolling]);
+
+  const getImageSource = () => {
+    switch (currentImage) {
+      case 1:
+        return image1;
+      case 2:
+        return image2;
+      case 3:
+        return image3;
+      case 4:
+        return image4;
+      case 5:
+        return image5;
+      default:
+        return image1;
+    }
+  };
 
   return (
     <div>
-      <img
-        src={`src/images/image${currentImage}.jpg`}
-        alt={`image ${currentImage}`}
-      />
+      <img src={getImageSource()} alt={`image ${currentImage}`} />
     </div>
   );
 };
