ce83f6b82ddff38eb01e539db421473ceb9fca72
epinark <122348695+epinark@users.noreply.github.com>
2023-11-29 16:02:00 +0100

    add new project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,6 +5,7 @@ import drtime from "../images/drtime.png";
 import myip from "../images/myip.png";
 import m from "../images/m.png";
 import quiz from "../images/quiz.png";
+import pokefight from "../images/pokefight.png";
 
 function Portfolio() {
   const { t } = useTranslation();
@@ -29,12 +30,18 @@ function Portfolio() {
                     <div className="project-card-inner">
                       <span className="description">
                         {t("drtime")}
+                        <span className="demo-account">
+                          <br />
+                          Demo Account: <br /> E-Mail: testaccount@test.com
+                          <br /> Password: 123456789
+                        </span>
                         <ul className="skills">
                           <li className="skill">React</li>
                           <li className="skill">Node.js</li>
                           <li className="skill">MongoDB</li>
                           <li className="skill">Express</li>
                           <li className="skill">Json Web Token</li>
+                          {/* <li className="skill">React Router Dom</li> */}
                         </ul>
                       </span>
                     </div>
@@ -81,7 +88,109 @@ function Portfolio() {
               </div>
             }
           />
-
+          <Section
+            id="project-card-drtime"
+            content={
+              <div className="animation">
+                <div className="project-name">
+                  <h2>PokeFight</h2>
+                  <div className="project-card-reverse">
+                    <div className="project-card-inner">
+                      <span className="description">
+                        {t("pokefight")}
+                        <ul className="skills">
+                          <li className="skill">React</li>
+                          <li className="skill">Node.js</li>
+                          <li className="skill">MongoDB</li>
+                          <li className="skill">API</li>
+                          <li className="skill">Tailwind</li>
+                        </ul>
+                      </span>
+                    </div>
+                    <div>
+                      <a
+                        className="image-container"
+                        href="https://pokefights.netlify.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          className="project-image"
+                          src={pokefight}
+                          alt="pokefight"
+                        />
+                        <span className="view">{t("viewproject")}⟶</span>
+                      </a>
+                      <div className="connections">
+                        <a
+                          href="https://pokefights.netlify.app/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <i className="fa-solid fa-up-right-from-square"></i>
+                        </a>
+                        <a
+                          href="https://github.com/epinark/pokefight"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <i className="fab fa-github"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            }
+          />
+          <Section
+            id="project-card-trivia"
+            content={
+              <div className="project-name">
+                <h2>Trivia-Quiz</h2>
+                <div className="project-card">
+                  <div className="project-card-inner">
+                    <span className="description">
+                      {t("trivia")}
+                      <ul className="skills">
+                        <li className="skill">React</li>
+                        <li className="skill">API</li>
+                      </ul>
+                    </span>
+                  </div>
+                  <div>
+                    <a
+                      className="image-container"
+                      href="https://justsomefun.netlify.app/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img className="project-image" src={quiz} alt="quiz" />
+                      <span className="view">{t("viewproject")}⟶</span>
+                    </a>
+                    <div className="connections">
+                      <a
+                        className="image-container"
+                        href="https://justsomefun.netlify.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fa-solid fa-up-right-from-square"></i>
+                      </a>
+                      <a
+                        className="image-container"
+                        href="https://github.com/epinark/Trivia-Quiz"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fab fa-github"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            }
+          />
           <Section
             id="project-card-myip"
             content={
@@ -93,7 +202,7 @@ function Portfolio() {
                       {t("myip")}
                       <ul className="skills">
                         <li className="skill">React</li>
-                        <li className="skill">Api</li>
+                        <li className="skill">API</li>
                         <li className="skill">Leaflet</li>
                       </ul>
                     </span>
@@ -122,54 +231,6 @@ function Portfolio() {
                       </a>
                       <a
                         href="https://github.com/epinark/my-ip"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fab fa-github"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            }
-          />
-          <Section
-            id="project-card-trivia"
-            content={
-              <div className="project-name">
-                <h2>Trivia-Quiz</h2>
-                <div className="project-card">
-                  <div className="project-card-inner">
-                    <span className="description">
-                      {t("trivia")}
-                      <ul className="skills">
-                        <li className="skill">React</li>
-                        <li className="skill">Api</li>
-                      </ul>
-                    </span>
-                  </div>
-                  <div>
-                    <a
-                      className="image-container"
-                      href="https://justsomefun.netlify.app/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img className="project-image" src={quiz} alt="quiz" />
-                      <span className="view">{t("viewproject")}⟶</span>
-                    </a>
-                    <div className="connections">
-                      <a
-                        className="image-container"
-                        href="https://justsomefun.netlify.app/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fa-solid fa-up-right-from-square"></i>
-                      </a>
-                      <a
-                        className="image-container"
-                        href="https://github.com/epinark/Trivia-Quiz"
                         target="_blank"
                         rel="noopener noreferrer"
                       >
