714def239529fd48a20575f590ee3e93d0e3f146
epinark <122348695+epinark@users.noreply.github.com>
2024-02-14 11:55:17 +0100

    add form with emails js with style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,42 @@
 import { useTranslation } from "react-i18next";
-import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const { t } = useTranslation();
   const contactRef = useRef();
   const isContactlRefInView = useInView(contactRef);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const text = "Say Hello";
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY
+      )
+
+      .then(
+        (response) => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        (error) => {
+          setError(true);
+          console.error("Email sending error:", error);
+        }
+      );
+  };
+
   return (
     <div className="contact-wrapper" id="contact" ref={contactRef}>
       <div className="contact-section">
@@ -16,25 +47,43 @@ function Contact() {
         >
           {t("contact")}
         </motion.h2>
-        <span className="border-c"></span>
-        <div className="contact-detail">
-          <a
-            className="contact"
-            href="mailto:esrapinarkaya@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="arrow">⟶</span>esrapinarkaya@gmail.com
-          </a>
-          <a
-            className="contact"
-            href="https://github.com/epinark"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="arrow">⟶</span>
-            github.com/epinark
-          </a>
+        {/* <span className="border-c"></span> */}
+        <div className="contact-container">
+          <div className="animation-container">
+            {" "}
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 5,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+          <div className="form-container">
+            {" "}
+            <form onSubmit={sendEmail} ref={form} className="">
+              <span>Dear Esra,</span>
+              <textarea rows={6} className="" name="user_message" />
+              <span>My mail address is:</span>
+              <input name="user_email" type="text" className="" />
+              <span>Regards</span>
+              <button className="send-button">Send</button>
+              {success && (
+                <span className="">
+                  Your message has been sent successfully!
+                </span>
+              )}
+              {error && <span className="">Something went wrong!</span>}
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {
+  defineConfig
+} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // Define your environment variables here
+    'process.env': process.env,
+  },
+});
